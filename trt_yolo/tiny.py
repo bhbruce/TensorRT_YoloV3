be69a1e4128a5b7609ea776a26ff7a13e2d74dea
summelon <52701013+summelon@users.noreply.github.com>
2020-06-16 13:15:43 +0800

    Add ./trt_yolo/tiny.py for run tiny YOLO on TRT

---
+++ trt_yolo/tiny.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python2
+#
+# Copyright 1993-2019 NVIDIA Corporation.  All rights reserved.
+#
+# NOTICE TO LICENSEE:
+#
+# This source code and/or documentation ("Licensed Deliverables") are
+# subject to NVIDIA intellectual property rights under U.S. and
+# international Copyright laws.
+#
+# These Licensed Deliverables contained herein is PROPRIETARY and
+# CONFIDENTIAL to NVIDIA and is being provided under the terms and
+# conditions of a form of NVIDIA software license agreement by and
+# between NVIDIA and Licensee ("License Agreement") or electronically
+# accepted by Licensee.  Notwithstanding any terms or conditions to
+# the contrary in the License Agreement, reproduction or disclosure
+# of the Licensed Deliverables to any third party without the express
+# written consent of NVIDIA is prohibited.
+#
+# NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+# LICENSE AGREEMENT, NVIDIA MAKES NO REPRESENTATION ABOUT THE
+# SUITABILITY OF THESE LICENSED DELIVERABLES FOR ANY PURPOSE.  IT IS
+# PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.
+# NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THESE LICENSED
+# DELIVERABLES, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY,
+# NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+# NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+# LICENSE AGREEMENT, IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY
+# SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY
+# DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+# OF THESE LICENSED DELIVERABLES.
+#
+# U.S. Government End Users.  These Licensed Deliverables are a
+# "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT
+# 1995), consisting of "commercial computer software" and "commercial
+# computer software documentation" as such terms are used in 48
+# C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government
+# only as a commercial end item.  Consistent with 48 C.F.R.12.212 and
+# 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995), all
+# U.S. Government End Users acquire the Licensed Deliverables with
+# only those rights set forth herein.
+#
+# Any use of the Licensed Deliverables in individual and commercial
+# software must include, in the user documentation and internal
+# comments to the code, the above Disclaimer and U.S. Government End
+# Users Notice.
+#
+
+from __future__ import print_function
+import tqdm
+import numpy as np
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+from PIL import ImageDraw
+
+from data_processing import  PostprocessYOLO
+
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import common
+
+from utils.utils import parse_data_cfg, load_classes, non_max_suppression, xywh2xyxy, box_iou,output_to_target,plot_images,ap_per_class
+from utils.data_loader import LoadImagesAndLabels, data_loader
+TRT_LOGGER = trt.Logger()
+
+import torch
+import time
+def draw_bboxes(image_raw, bboxes, confidences, categories, all_categories, bbox_color='blue'):
+    """Draw the bounding boxes on the original input image and return it.
+
+    Keyword arguments:
+    image_raw -- a raw PIL Image
+    bboxes -- NumPy array containing the bounding box coordinates of N objects, with shape (N,4).
+    categories -- NumPy array containing the corresponding category for each object,
+    with shape (N,)
+    confidences -- NumPy array containing the corresponding confidence for each object,
+    with shape (N,)
+    all_categories -- a list of all categories in the correct ordered (required for looking up
+    the category name)
+    bbox_color -- an optional string specifying the color of the bounding boxes (default: 'blue')
+    """
+    draw = ImageDraw.Draw(image_raw)
+    print(bboxes, confidences, categories)
+    for box, score, category in zip(bboxes, confidences, categories):
+        x_coord, y_coord, width, height = box
+        left = max(0, np.floor(x_coord + 0.5).astype(int))
+        top = max(0, np.floor(y_coord + 0.5).astype(int))
+        right = min(image_raw.width, np.floor(x_coord + width + 0.5).astype(int))
+        bottom = min(image_raw.height, np.floor(y_coord + height + 0.5).astype(int))
+
+        draw.rectangle(((left, top), (right, bottom)), outline=bbox_color)
+        draw.text((left, top - 12), '{0} {1:.2f}'.format(all_categories[category], score), fill=bbox_color)
+
+    return image_raw
+
+def get_engine(onnx_file_path, engine_file_path=""):
+    """Attempts to load a serialized engine if available, otherwise builds a new TensorRT engine and saves it."""
+    def build_engine():
+        """Takes an ONNX file and creates a TensorRT engine to run inference with"""
+        with trt.Builder(TRT_LOGGER) as builder, builder.create_network(common.EXPLICIT_BATCH) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
+            builder.max_workspace_size = 1 << 28 # 256MiB
+            builder.max_batch_size = 1
+            # Parse model file
+            builder.fp16_mode = True
+            if not os.path.exists(onnx_file_path):
+                print('ONNX file {} not found, please run yolov3_to_onnx.py first to generate it.'.format(onnx_file_path))
+                exit(0)
+            print('Loading ONNX file from path {}...'.format(onnx_file_path))
+            with open(onnx_file_path, 'rb') as model:
+                print('Beginning ONNX file parsing')
+                if not parser.parse(model.read()):
+                    print ('ERROR: Failed to parse the ONNX file.')
+                    for error in range(parser.num_errors):
+                        print (parser.get_error(error))
+                    return None
+            # The actual yolov3.onnx is generated with batch size 64. Reshape input to batch size 1
+            network.get_input(0).shape = [16, 3, 448, 448]# 608, 608]
+            print('Completed parsing of ONNX file')
+            print('Building an engine from file {}; this may take a while...'.format(onnx_file_path))
+            engine = builder.build_cuda_engine(network)
+            print("Completed creating Engine")
+            with open(engine_file_path, "wb") as f:
+                f.write(engine.serialize())
+            return engine
+
+    if os.path.exists(engine_file_path):
+        # If a serialized engine exists, use it instead of building an engine.
+        print("Reading engine from file {}".format(engine_file_path))
+        with open(engine_file_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
+            return runtime.deserialize_cuda_engine(f.read())
+    else:
+        return build_engine()
+
+def main():
+    """Create a TensorRT engine for ONNX-based YOLOv3-608 and run inference."""
+
+    # Try to load a previously generated YOLOv3-608 network graph in ONNX format:
+    onnx_file_path = './onnx/tiny.onnx'
+    engine_file_path = "./engine/tiny.trt"
+    data_path = "/home/train/data/unrel.data"
+
+    data = parse_data_cfg(data_path)
+    nc = int(data['classes'])  # number of classes
+    path = data['valid']  # path to test images
+    names = load_classes(data['names'])  # class names
+
+    iouv =  torch.linspace(0.5, 0.95, 1, dtype=torch.float32)  # iou vector for mAP@0.5:0.95
+    niou = 1
+
+    conf_thres = 0.001
+    iou_thres = 0.6
+    verbose = True
+
+    # Genearte custom dataloader
+    img_size = 448 # copy form pytorch src
+    batch_size = 16
+
+    dataset = LoadImagesAndLabels(path, img_size, batch_size, rect=False)
+    batch_size = min(batch_size, len(dataset))
+    dataloader = data_loader(dataset, batch_size, img_size)
+
+    # Output shapes expected by the post-processor
+    output_shapes = [(16, 126, 14, 14), (16, 126, 28, 28)]
+
+    # Do inference with TensorRT
+    trt_outputs = []
+    with get_engine(onnx_file_path, engine_file_path) as engine, engine.create_execution_context() as context:
+        inputs, outputs, bindings, stream = common.allocate_buffers(engine)
+        s = ('%20s' + '%10s' * 6) % ('Class', 'Images', 'Targets', 'P', 'R', 'mAP@0.5', 'F1')
+        p, r, f1, mp, mr, map, mf1, t0, t1 = 0., 0., 0., 0., 0., 0., 0., 0., 0.
+        pbar = tqdm.tqdm(dataloader, desc=s)
+        stats, ap, ap_class =  [], [], []
+        seen = 0
+        postprocessor_args = {"yolo_masks":   [(3, 4, 5), (0, 1, 2)],                  # A list of 3 three-dimensional tuples for the YOLO masks
+                              "yolo_anchors": [(10, 14), (23, 27), (37, 58), (81, 82), (135, 169), (344, 319)],
+                              "num_classes": 37,
+                              "stride":[32, 16, 8]}
+
+        postprocessor = PostprocessYOLO(**postprocessor_args)
+        for batch_i, (imgs, targets, paths, shapes) in enumerate(pbar):
+            imgs = imgs.astype(np.float32) / 255.0
+            nb, _, height, width = imgs.shape  # batch size, channels, height, width
+            whwh = np.array([width, height, width, height])
+
+            inputs[0].host = imgs
+
+            # Do layers before yolo
+            t = time.time()
+            trt_outputs = common.do_inference_v2(context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
+
+            trt_outputs = [output.reshape(shape) for output, shape in zip(trt_outputs, output_shapes)]
+
+            trt_outputs = [np.ascontiguousarray(otpt[:, :, :int(imgs.shape[2]*(2**i)/32), :int(imgs.shape[3]*(2**i)/32)], dtype=np.float32) for i, otpt in enumerate(trt_outputs)]
+
+
+            output_list = postprocessor.process(trt_outputs)
+
+            t0 += time.time() - t
+
+            inf_out = torch.cat(output_list, 1)
+            t = time.time()
+            output = non_max_suppression(inf_out, conf_thres=conf_thres, iou_thres=iou_thres)  # nms
+            t1 += time.time() - t
+
+            # Statistics per image
+            for si, pred in enumerate(output):
+                labels = targets[targets[:, 0] == si, 1:]
+                nl = len(labels)
+                tcls = labels[:, 0].tolist() if nl else []  # target class
+                seen += 1
+
+                if pred is None:
+                    if nl:
+                        stats.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
+                    continue
+
+                # Assign all predictions as incorrect
+                correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool)
+                if nl:
+                    detected = []  # target indices
+                    tcls_tensor = labels[:, 0]
+
+                    # target boxes
+                    tbox = xywh2xyxy(labels[:, 1:5]) * whwh
+                    tbox = tbox.type(torch.float32)
+
+                    # Per target class
+                    for cls in torch.unique(tcls_tensor):
+                        ti = (cls == tcls_tensor).nonzero().view(-1)  # prediction indices
+                        pi = (cls == pred[:, 5]).nonzero().view(-1)  # target indices
+
+                        # Search for detections
+                        if pi.shape[0]:
+                            # Prediction to target ious
+                            ious, i = box_iou(pred[pi, :4], tbox[ti]).max(1)  # best ious, indices
+
+                            # Append detections
+                            for j in (ious > iouv[0]).nonzero():
+                                d = ti[i[j]]  # detected target
+                                if d not in detected:
+                                    detected.append(d)
+                                    correct[pi[j]] = ious[j] > iouv  # iou_thres is 1xn
+                                    if len(detected) == nl:  # all targets already located in image
+                                        break
+
+                # Append statistics (correct, conf, pcls, tcls)
+                stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))
+
+            # Plot images
+            if batch_i < 1:
+                f = 'test_batch%g_gt.jpg' % batch_i  # filename
+                plot_images(imgs, targets, paths=paths, names=names, fname=f)  # ground truth
+                f = 'test_batch%g_pred.jpg' % batch_i
+                plot_images(imgs, output_to_target(output, width, height), paths=paths, names=names, fname=f)  # predictions
+
+        # Compute statistics
+        stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
+        if len(stats):
+            p, r, ap, f1, ap_class = ap_per_class(*stats)
+            if niou > 1:
+                p, r, ap, f1 = p[:, 0], r[:, 0], ap.mean(1), ap[:, 0]  # [P, R, AP@0.5:0.95, AP@0.5]
+            mp, mr, map, mf1 = p.mean(), r.mean(), ap.mean(), f1.mean()
+            nt = np.bincount(stats[3].astype(np.int64), minlength=nc)  # number of targets per class
+        else:
+            nt = torch.zeros(1)
+
+        # Print results
+        pf = '%20s' + '%10.3g' * 6  # print format
+        print(pf % ('all', seen, nt.sum(), mp, mr, map, mf1))
+
+        # Print results per class
+        if verbose and nc > 1 and len(stats):
+            for i, c in enumerate(ap_class):
+                print(pf % (names[c], seen, nt[c], p[i], r[i], ap[i], f1[i]))
+
+        # Print speeds
+        if verbose :
+            t = tuple(x / seen * 1E3 for x in (t0, t1, t0 + t1)) + (img_size, img_size, batch_size)  # tuple
+            print('Speed: %.1f/%.1f/%.1f ms inference/NMS/total per %gx%g image at batch-size %g' % t)
+
+
+if __name__ == '__main__':
+    main()
